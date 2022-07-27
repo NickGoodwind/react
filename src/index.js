decce987e1b6c23a3b960612da8a781a8c6d4121
NickGoodwind <nickgoodwind@gmail.com>
2022-07-27 14:08:37 -0500

    React setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const Content = () => {
+  const [page, setPage] = React.useState();
+
+  return (
+    <div>
+      This is the test page
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Content />
 );
 
 // If you want to start measuring performance in your app, pass a function
